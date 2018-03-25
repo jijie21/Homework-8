655e71d81ba4f9e964d4129d1231a579e337d53b
Jie(Jackie) Ji <35789241+jijie21@users.noreply.github.com>
2018-03-25 01:17:09 -0700

    Update wrong-turn.ino

--- wrong-turn.ino
+++ wrong-turn.ino
@@ -1,22 +1,21 @@
+
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
-  pinMode(5, OUTPUT);
+  pinMode(9, OUTPUT);
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  for (int i = 0; i < 3; i++) {
-digitalWrite(5, HIGH);
+for (int i = 0; i < 3; i++) {
+digitalWrite(9, HIGH);
 delay(300);
-digitalWrite(5, LOW);
+digitalWrite(9, LOW);
 delay(300);
 }
-for (int j = 0; j < 10; j++) {
-for (int k = 0; k < 20; k++) {
-digitalWrite(5, HIGH);
-delay(200-k*10);
-digitalWrite(5, LOW);
-delay(200-k*10);
-}
+for (int b = 0; b < 20; b++) {
+digitalWrite(9, HIGH);
+delay(200-b*10);
+digitalWrite(9, LOW);
+delay(200-b*10);
 }
 }
