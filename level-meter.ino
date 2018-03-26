c01d45f8106e0d037e5a40251d1f61f81911547e
Jie(Jackie) Ji <35789241+jijie21@users.noreply.github.com>
2018-03-26 08:37:24 -0700

    Update level-meter.ino

--- level-meter.ino
+++ level-meter.ino
@@ -1,31 +1,41 @@
-int sensorValue = 0;  
-int sensorPin = A0;    
-int brightness = 0;
+int sensorValue = 0;  // variable to store the value coming from the sensor
+int sensorPin = A0;    // select the input pin for the potentiometer
 
+// the setup function runs once when you press reset or power the board
 void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
   pinMode(9, OUTPUT);
+
   Serial.begin(9600);
 }
 
-// the loop function runs over and over again forever
+
 void loop() {
   
   sensorValue = analogRead(sensorPin);
 
   if (sensorValue == 1023) {
+    digitalWrite(5, LOW);
+    digitalWrite(6, LOW);
     digitalWrite(9, LOW);
     sensorValue = analogRead(sensorPin);
-  } else if (sensorValue <= 256) {
-    digitalWrite(9, HIGH);
-      brightness = brightness + sensorValue;
+  } else if (sensorValue > 0 && sensorValue <= 255) {
+    digitalWrite(5, HIGH);
+     digitalWrite(6, LOW);
+    digitalWrite(9, LOW);
     sensorValue = analogRead(sensorPin);
-  } else if (sensorValue <= 512) {
-    digitalWrite(9, HIGH);
-      brightness = brightness + sensorValue;
+  } else if (sensorValue > 255 && sensorValue <= 511) {
+    digitalWrite(5, HIGH);
+     digitalWrite(6, HIGH);
+    digitalWrite(9, LOW);
     sensorValue = analogRead(sensorPin);
-  } else if (sensorValue <= 767) {
-    digitalWrite(9, HIGH);
-      brightness = brightness + sensorValue;
+  } else if (sensorValue > 511 && sensorValue <= 767) {
+    digitalWrite(5, HIGH);
+     digitalWrite(6, HIGH);
+     digitalWrite(9, HIGH);
+
     sensorValue = analogRead(sensorPin);
   }
   
